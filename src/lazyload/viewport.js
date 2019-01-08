81f6fa808895b80f47cb2301fd556b8110400d1a
zenxds <zenxds@gmail.com>
2019-01-08 10:49:21 +0800

    prettier

--- src/lazyload/viewport.js
+++ src/lazyload/viewport.js
@@ -1,11 +1,19 @@
 // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
 
 function getViewportWidth() {
-  return window.innerWidth || (document.documentElement && document.documentElement.clientWidth) || document.body.clientWidth
+  return (
+    window.innerWidth ||
+    (document.documentElement && document.documentElement.clientWidth) ||
+    document.body.clientWidth
+  )
 }
 
 function getViewportHeight() {
-  return window.innerHeight || (document.documentElement && document.documentElement.clientHeight) || document.body.clientHeight
+  return (
+    window.innerHeight ||
+    (document.documentElement && document.documentElement.clientHeight) ||
+    document.body.clientHeight
+  )
 }
 
 /**
@@ -27,11 +35,12 @@ function isElementInViewport(el) {
 /**
  * 元素任一部分在视口内
  */
-export function isAnyPartOfElementInViewport(el, threshold=0) {
+export function isAnyPartOfElementInViewport(el, threshold = 0) {
   const rect = el.getBoundingClientRect()
 
   const viewportHeight = getViewportHeight()
-  const verticalInView = (rect.top - threshold <= viewportHeight) && (rect.bottom + threshold >= 0)
+  const verticalInView =
+    rect.top - threshold <= viewportHeight && rect.bottom + threshold >= 0
 
   return verticalInView
 }
